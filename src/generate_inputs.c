23a3d44cf8d6eb873466bb33c9cd452e9d205e50
Felipe Sasdelli <sasdelli.pi@gmail.com>
2023-11-18 17:02:41 -0300

    Generating outputs

--- src/generate_inputs.c
+++ src/generate_inputs.c
@@ -1,9 +1,30 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
+
+void create_directory(const char *path) {
+    char tmp[256];
+    char *p = NULL;
+    size_t len;
+
+    snprintf(tmp, sizeof(tmp), "%s", path);
+    len = strlen(tmp);
+    if (tmp[len - 1] == '/') {
+        tmp[len - 1] = 0;
+    }
+    for (p = tmp + 1; *p; p++) {
+        if (*p == '/') {
+            *p = 0;
+            mkdir(tmp, S_IRWXU);
+            *p = '/';
+        }
+    }
+    mkdir(tmp, S_IRWXU);
+}
 
 void generate_instance(const char *foldername, const char *filename, int n,
                        int W) {
@@ -28,28 +49,27 @@ void generate_instance(const char *foldername, const char *filename, int n,
 
 int main() {
     srand(time(NULL));
-    const char *foldername = "data/inputs";
+    const char *input_folder = "data/inputs";
+    const char *output_folder = "data/outputs";
     int n_fixed = 400;    // Número fixo de itens
     int W_variable = 100; // Capacidade inicial da mochila que irá variar
 
-    // Criar o diretório 'inputs', se não existir
-    if (mkdir(foldername, 0777) != 0 && errno != EEXIST) {
-        perror("Erro ao criar o diretório 'inputs'");
-        exit(EXIT_FAILURE);
-    }
+    // Criar os diretórios para os arquivos de entrada e saída, caso não existam
+    create_directory(input_folder);
+    create_directory(output_folder);
 
     // Gerar 20 instâncias com n variando
     for (int n = 100, i = 1; i <= 20; n *= 2, ++i) {
         char filename[50];
         sprintf(filename, "input-%d-n.txt", n);
-        generate_instance(foldername, filename, n, 100);
+        generate_instance(input_folder, filename, n, 100);
     }
 
     // Gerar 20 instâncias com W variando e n fixo em 400
     for (int i = 1; i <= 20; W_variable *= 2, ++i) {
         char filename[50];
         sprintf(filename, "input-%d-W.txt", W_variable);
-        generate_instance(foldername, filename, n_fixed, W_variable);
+        generate_instance(input_folder, filename, n_fixed, W_variable);
     }
 
     return 0;
--- src/main.c
+++ src/main.c
@@ -10,138 +10,145 @@
 #include "../include/branch_and_bound.h"
 #include "../include/dynamic.h"
 
-void run_algorithm(const char *input_dir, const char *output_dir,
-                   const char *algorithm_name) {
-    DIR *dir = opendir(input_dir);
-    if (dir == NULL) {
-        perror("Erro ao abrir o diretório de entrada");
-        exit(EXIT_FAILURE);
-    }
+typedef struct {
+    char name[256];
+    int size;
+} FileEntry;
 
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_REG) { // Verifica se é um arquivo regular
-            printf("Processando instância: %s\n", entry->d_name);
+int compare_file_entries(const void *a, const void *b) {
+    FileEntry *entry_a = (FileEntry *)a;
+    FileEntry *entry_b = (FileEntry *)b;
+    return entry_a->size - entry_b->size;
+}
 
-            char input_filepath[256], output_filepath[256],
-                output_folderpath[256];
-            int ret;
+void extract_size_from_name(const char *filename, int *size) {
+    sscanf(filename, "input-%d-", size);
+}
 
-            ret = snprintf(input_filepath, sizeof(input_filepath), "%s/%s",
-                           input_dir, entry->d_name);
-            if (ret >= sizeof(input_filepath) || ret < 0) {
-                fprintf(stderr,
-                        "Erro ao formatar o caminho do arquivo de entrada.\n");
-                continue;
-            }
+void process_files_in_order(FileEntry *entries, int count,
+                            const char *input_dir, const char *output_dir,
+                            const char *algorithm_name) {
+    for (int i = 0; i < count; i++) {
+        printf("Processando instância: %s\n", entries[i].name);
 
-            ret = snprintf(output_folderpath, sizeof(output_folderpath),
-                           "%s/%s", output_dir, algorithm_name);
-            if (ret >= sizeof(output_folderpath) || ret < 0) {
-                fprintf(stderr,
-                        "Erro ao formatar o caminho da pasta de saída.\n");
-                continue;
-            }
+        char input_filepath[256], output_filepath[256], output_folderpath[256],
+            output_filename[256];
+        int ret;
 
-            mkdir(output_folderpath,
-                  0777); // Cria o diretório de saída se não existir
+        ret = snprintf(input_filepath, sizeof(input_filepath), "%s/%s",
+                       input_dir, entries[i].name);
+        if (ret >= sizeof(input_filepath) || ret < 0) {
+            fprintf(stderr,
+                    "Erro ao formatar o caminho do arquivo de entrada.\n");
+            continue;
+        }
 
-            ret = snprintf(output_filepath, sizeof(output_filepath), "%s/%s",
-                           output_folderpath, entry->d_name);
-            if (ret >= sizeof(output_filepath) || ret < 0) {
-                fprintf(stderr,
-                        "Erro ao formatar o caminho do arquivo de saída.\n");
-                continue;
-            }
+        ret = snprintf(output_folderpath, sizeof(output_folderpath), "%s/%s",
+                       output_dir, algorithm_name);
+        if (ret >= sizeof(output_folderpath) || ret < 0) {
+            fprintf(stderr, "Erro ao formatar o caminho da pasta de saída.\n");
+            continue;
+        }
 
-            FILE *input_file = fopen(input_filepath, "r");
-            if (input_file == NULL) {
-                perror("Erro ao abrir o arquivo de entrada");
-                continue;
-            }
+        mkdir(output_folderpath, 0777);
 
-            int W, n = 0, weight, value;
-            ret = fscanf(input_file, "%d", &W); // Lê a capacidade da mochila
-            if (ret != 1) {
-                perror("Erro ao ler a capacidade da mochila do arquivo de "
-                       "entrada");
-                fclose(input_file);
-                continue;
-            }
+        ret = snprintf(output_filename, sizeof(output_filename), "output%s",
+                       strchr(entries[i].name, '-'));
+        if (ret >= sizeof(output_filename) || ret < 0) {
+            fprintf(stderr, "Erro ao formatar o nome do arquivo de saída.\n");
+            continue;
+        }
 
-            // Conta o número de itens
-            while ((ret = fscanf(input_file, "%d %d", &weight, &value)) == 2) {
-                n++;
-            }
+        ret = snprintf(output_filepath, sizeof(output_filepath), "%s/%s",
+                       output_folderpath, output_filename);
+        if (ret >= sizeof(output_filepath) || ret < 0) {
+            fprintf(stderr,
+                    "Erro ao formatar o caminho do arquivo de saída.\n");
+            continue;
+        }
 
-            // Verifica se o último fscanf falhou sem ser o fim do arquivo
-            if (ret != EOF) {
-                perror("Erro ao ler os pesos e valores do arquivo de entrada");
-                fclose(input_file);
-                continue;
-            }
+        FILE *input_file = fopen(input_filepath, "r");
+        if (input_file == NULL) {
+            perror("Erro ao abrir o arquivo de entrada");
+            continue;
+        }
 
-            int *weights = malloc(n * sizeof(int));
-            int *values = malloc(n * sizeof(int));
-            Item *items = malloc(n * sizeof(Item));
+        int W, n = 0, weight, value;
+        ret = fscanf(input_file, "%d", &W);
+        if (ret != 1) {
+            perror("Erro ao ler a capacidade da mochila do arquivo de entrada");
+            fclose(input_file);
+            continue;
+        }
 
-            rewind(input_file); // Volta ao início do arquivo
-            ret = fscanf(input_file, "%d",
-                         &W); // Lê a capacidade da mochila novamente
-            if (ret != 1) {
-                perror("Erro ao ler a capacidade da mochila do arquivo de "
-                       "entrada");
-                fclose(input_file);
-                free(weights);
-                free(values);
-                free(items);
-                continue;
-            }
+        while ((ret = fscanf(input_file, "%d %d", &weight, &value)) == 2) {
+            n++;
+        }
 
-            for (int i = 0; i < n; i++) {
-                ret = fscanf(input_file, "%d %d", &weights[i], &values[i]);
-                if (ret != 2) {
-                    perror("Erro ao ler pesos e valores dos itens do arquivo "
-                           "de entrada");
-                    break;
-                }
-                items[i].weight = weights[i];
-                items[i].value = values[i];
-            }
+        if (ret != EOF) {
+            perror("Erro ao ler os pesos e valores do arquivo de entrada");
+            fclose(input_file);
+            continue;
+        }
 
-            fclose(input_file); // Fecha o arquivo de entrada
+        int *weights = malloc(n * sizeof(int));
+        int *values = malloc(n * sizeof(int));
+        Item *items = malloc(n * sizeof(Item));
 
-            clock_t start = clock();
-            int max_profit = 0;
-            if (strcmp(algorithm_name, "dynamic") == 0) {
-                max_profit = knapsack_dynamic(W, weights, values, n);
-            } else if (strcmp(algorithm_name, "backtracking") == 0) {
-                max_profit =
-                    knapsack_backtracking(W, weights, values, n, 0, 0, 0);
-            } else if (strcmp(algorithm_name, "branch_and_bound") == 0) {
-                max_profit = knapsack_branch_and_bound(W, items, n);
-            }
-            clock_t end = clock();
-            double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-
-            FILE *output_file = fopen(output_filepath, "w");
-            if (output_file == NULL) {
-                perror("Erro ao abrir o arquivo de saída");
-                continue;
-            }
-
-            fprintf(output_file,
-                    "Lucro máximo: %d\nTempo de execução: %.2f segundos\n",
-                    max_profit, cpu_time_used);
-            fclose(output_file); // Fecha o arquivo de saída
-
+        rewind(input_file);
+        ret = fscanf(input_file, "%d", &W);
+        if (ret != 1) {
+            perror("Erro ao ler a capacidade da mochila do arquivo de entrada");
+            fclose(input_file);
             free(weights);
             free(values);
             free(items);
+            continue;
         }
-    }
 
-    closedir(dir); // Fecha o diretório de entrada
+        for (int i = 0; i < n; i++) {
+            ret = fscanf(input_file, "%d %d", &weights[i], &values[i]);
+            if (ret != 2) {
+                perror("Erro ao ler pesos e valores dos itens do arquivo de "
+                       "entrada");
+                break;
+            }
+            items[i].weight = weights[i];
+            items[i].value = values[i];
+        }
+
+        fclose(input_file);
+
+        clock_t start = clock();
+        int max_profit = 0;
+        if (strcmp(algorithm_name, "dynamic") == 0) {
+            max_profit = knapsack_dynamic(W, weights, values, n);
+        } else if (strcmp(algorithm_name, "backtracking") == 0) {
+            max_profit = knapsack_backtracking(W, weights, values, n, 0, 0, 0);
+        } else if (strcmp(algorithm_name, "branch_and_bound") == 0) {
+            max_profit = knapsack_branch_and_bound(W, items, n);
+        }
+        clock_t end = clock();
+        double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        FILE *output_file = fopen(output_filepath, "w");
+        if (output_file == NULL) {
+            perror("Erro ao abrir o arquivo de saída");
+            free(weights);
+            free(values);
+            free(items);
+            continue;
+        }
+
+        fprintf(output_file,
+                "Lucro máximo: %d\nTempo de execução: %.2f segundos\n",
+                max_profit, cpu_time_used);
+        fclose(output_file);
+
+        free(weights);
+        free(values);
+        free(items);
+    }
 }
 
 int main() {
@@ -155,7 +162,31 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    run_algorithm(input_dir, output_dir, algorithm_name);
+    DIR *dir = opendir(input_dir);
+    if (dir == NULL) {
+        perror("Erro ao abrir o diretório de entrada");
+        return EXIT_FAILURE;
+    }
+
+    FileEntry *entries = NULL;
+    int count = 0;
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_REG) {
+            count++;
+            entries = realloc(entries, count * sizeof(FileEntry));
+            strcpy(entries[count - 1].name, entry->d_name);
+            extract_size_from_name(entry->d_name, &entries[count - 1].size);
+        }
+    }
+    closedir(dir);
+
+    qsort(entries, count, sizeof(FileEntry), compare_file_entries);
+
+    process_files_in_order(entries, count, input_dir, output_dir,
+                           algorithm_name);
+
+    free(entries);
 
     return 0;
 }
